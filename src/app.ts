f4a2d174b2fd7a319604815b0c801436ed6dd7fa
axel-seugling <axel.seugling@inter.ikea.com>
2022-12-08 13:38:41 +0100

    feat: 10 is the new number

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ console.log("Testing fix in GIT. Feature. Breaking change. behind. New feature")
 console.log("Some new text");
 
 const run = () => {
-    return 2;
+    return 10;
 }
 
 console.log(run());
